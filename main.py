986c66f6d19cb4d3860ec9e83201f9294d5f2812
Chris <1411356992@qq.com>
2018-02-11 17:59:02 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     main
+   Description :
+   Author :       Chris
+   date：          2018/2/11
+-------------------------------------------------
+"""
+import menu
+
+if __name__ == "__main__":
+    menu.start()
